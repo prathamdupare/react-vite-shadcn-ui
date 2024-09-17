6707bec43c98ff4ecabb41b59be527d7908e53b0
prathamdupare <prathamdupare@proton.me>
2024-09-17 14:07:30 +0530

    updated  theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ThemeProvider defaultTheme="light">
       <App />
     </ThemeProvider>
   </React.StrictMode>,
